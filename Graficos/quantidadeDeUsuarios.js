f03583543f5193d17c4d33ae6eb9de149c54866c
Luana Vitória Nerilo Bilha De Souza <luana.bilha.souza@escola.pr.gov.br>
2024-10-25 19:14:17 -0300

    14-10

--- Graficos/quantidadeDeUsuarios.js
+++ Graficos/quantidadeDeUsuarios.js
@@ -16,6 +16,27 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-
         }
       }
   ]
+    const layout = {
+      plot_bgcolor : getCSS('--cor-de-fundo'),
+      paper_bgcolor : getCSS ('--cor-de-fundo'),
+      title : {
+        text: 'Redes sociais com mais usuario no mundo',
+        x: 0,
+        font: {
+        color: getCSS('--cor-primaria'),
+        family: getCSS('--fonte'),
+        size: 30
+
+
+
+
+        
+        
+
+        }
+      }
+    }
+ 
   const grafico = document.createElement('div');
   grafico.className = 'grafico';
   document.getElementById('graficos-container').appendChild(grafico);
